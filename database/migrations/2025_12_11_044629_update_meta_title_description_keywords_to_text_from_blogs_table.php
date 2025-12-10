09bae1d7387941a4b85defa640c0e6e5d0598813
kevinpauneljohn <johnkevinpaunel@gmail.com>
2025-12-11 04:50:19 +0800

    update the blogs and properties meta columns

---
+++ database/migrations/2025_12_11_044629_update_meta_title_description_keywords_to_text_from_blogs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->text('meta_title')->nullable()->change();
+            $table->text('meta_keywords')->nullable()->change();
+            $table->text('meta_description')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->string('meta_title')->nullable()->change();
+            $table->string('meta_keywords')->nullable()->change();
+            $table->string('meta_description')->nullable()->change();
+        });
+    }
+};
