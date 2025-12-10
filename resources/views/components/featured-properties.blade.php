8f668694cf6a0897e2c78e221714af042c91d423
kevinpauneljohn <johnkevinpaunel@gmail.com>
2025-12-10 13:55:54 +0800

    check error

--- resources/views/components/featured-properties.blade.php
+++ resources/views/components/featured-properties.blade.php
@@ -1,5 +1,5 @@
 <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-    @foreach($properties as $property)
+    @foreach($featuredProperties as $property)
         <x-properties.property-card :property="$property"/>
     @endforeach
 </div>
