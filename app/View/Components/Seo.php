c93f55563ce3a45d40379f70d6a35130c721d9a1
kevinpauneljohn <johnkevinpaunel@gmail.com>
2025-12-19 01:39:54 +0800

    update htaccess

--- app/View/Components/Seo.php
+++ app/View/Components/Seo.php
@@ -31,8 +31,8 @@ class Seo extends Component
         $this->keywords = $keywords ?? 'pampanga real estate, house and lot for sale, angeles city, clark, dream home guide realty';
         $this->image = $image ?? asset('images/logo.png');
         $this->schemaType = $schemaType;
-//        $this->canonical = $canonical ?? url()->current();
-        $this->canonical = 'https://johnkevinpaunel.com';
+        $this->canonical = $canonical ?? url()->current();
+//        $this->canonical = 'https://johnkevinpaunel.com';
     }
 
     /**
