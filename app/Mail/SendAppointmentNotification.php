7e8c6fe099e642b04a7f878d963a88624b2338e1
kevinpauneljohn <johnkevinpaunel@gmail.com>
2025-12-21 14:29:05 +0800

    mailables

--- app/Mail/SendAppointmentNotification.php
+++ app/Mail/SendAppointmentNotification.php
@@ -11,7 +11,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendAppointmentNotification extends Mailable
+class SendAppointmentNotification extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
@@ -24,7 +24,7 @@ class SendAppointmentNotification extends Mailable
         public Appointment $appointment
     )
     {
-//        $this->afterCommit();
+        $this->afterCommit();
     }
 
     /**
