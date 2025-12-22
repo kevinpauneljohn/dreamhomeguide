4d968066504cd90a6086daac200ba74a4a206ad1
kevinpauneljohn <johnkevinpaunel@gmail.com>
2025-12-22 12:13:18 +0800

    update about us

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -11,7 +11,7 @@ class AboutUsController extends Controller
     {
         return view('pages.about-us')->with([
             'title' => 'About Us',
-            'teams' => User::where('position','!=',null)->orWhere('position','!=',''),
+            'teams' => User::where('position','!=',null)->where('position','!=',''),
         ]);
     }
 }
