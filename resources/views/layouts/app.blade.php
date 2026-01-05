2121e181b4c3c396f498bf318b2c546abc1437e0
kevinpauneljohn <johnkevinpaunel@gmail.com>
2026-01-05 11:34:35 +0800

    fb chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,33 +41,38 @@
     <!-- Facebook Messenger Chat Plugin -->
     <div id="fb-root"></div>
 
-    <div id="fb-customer-chat"
-         class="fb-customerchat"
-         attribution="biz_inbox">
+    <script async defer crossorigin="anonymous"
+            src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v19.0">
+    </script>
+
+    <div class="fb-customerchat"
+         attribution="biz_inbox"
+         page_id="1928768050502869">
     </div>
+
 
     <script>
         var chatbox = document.getElementById('fb-customer-chat');
         chatbox.setAttribute("page_id", "1928768050502869");
     </script>
 
-    <script>
-        window.fbAsyncInit = function () {
-            FB.init({
-                xfbml: true,
-                version: 'v19.0'
-            });
-        };
+{{--    <script>--}}
+{{--        window.fbAsyncInit = function () {--}}
+{{--            FB.init({--}}
+{{--                xfbml: true,--}}
+{{--                version: 'v19.0'--}}
+{{--            });--}}
+{{--        };--}}
 
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
+{{--        (function (d, s, id) {--}}
+{{--            var js, fjs = d.getElementsByTagName(s)[0];--}}
+{{--            if (d.getElementById(id)) return;--}}
+{{--            js = d.createElement(s);--}}
+{{--            js.id = id;--}}
+{{--            js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";--}}
+{{--            fjs.parentNode.insertBefore(js, fjs);--}}
+{{--        }(document, 'script', 'facebook-jssdk'));--}}
+{{--    </script>--}}
 
 </body>
 </html>
