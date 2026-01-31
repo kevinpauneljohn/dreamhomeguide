069d012731e778b35c8abb1859e3bc03148b8d49
kevinpauneljohn <johnkevinpaunel@gmail.com>
2026-01-31 12:06:54 +0800

    capacitor

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,24 @@ import '../css/custom.css'
 import $ from 'jquery';
 window.$ = window.jQuery = $;
 
+document.addEventListener('DOMContentLoaded', async () => {
+
+    // Capacitor exists ONLY inside mobile app
+    if (window.Capacitor?.getPlatform?.() === 'android') {
+
+        // Dynamically access plugin (no import!)
+        const { StatusBar } = window.Capacitor.Plugins || {};
+
+        if (StatusBar) {
+            await StatusBar.setOverlaysWebView({ overlay: false });
+        }
+
+        document.body.classList.add('android');
+    }
+
+});
+
+
 
 
 
