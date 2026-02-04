1460a22f82ac91d75a12ff294af5faebdac66d8c
kevinpauneljohn <johnkevinpaunel@gmail.com>
2026-02-04 22:46:47 +0800

    delete sales

--- database/migrations/2026_01_31_221529_create_commissions_table.php
+++ database/migrations/2026_01_31_221529_create_commissions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('project_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->float('rate');
             $table->timestamps();
         });
     }
