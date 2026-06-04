149e5f1bfe21aa031192e4ee8a3b4a2b6aa0524b
kevinpauneljohn <johnkevinpaunel@gmail.com>
2026-06-04 20:15:20 +0800

    fix submit model unit

--- database/migrations/2026_06_04_201218_drop_unique_to_slug_column_from_model_units_table.php
+++ database/migrations/2026_06_04_201218_drop_unique_to_slug_column_from_model_units_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('model_units', function (Blueprint $table) {
-            $table->dropUnique('slug');
+            $table->dropUnique(['slug']);
         });
     }
 
